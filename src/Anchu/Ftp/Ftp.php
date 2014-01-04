7089f1f05c6913608ce9700d0f73faa76d167c61
Harish Anchu <harishanchu@gmail.com>
2014-01-05 03:55:58 +0530

    constructor and basic methods added

--- src/Anchu/Ftp/Ftp.php
+++ src/Anchu/Ftp/Ftp.php
@@ -2,4 +2,50 @@
 
 class Ftp {
 
+    /**
+     * The active FTP connection resource id.
+     */
+    protected $connectionId;
+
+    /**
+     * Create a new ftp connection instance.
+     *
+     * @param  config
+     * @return void
+     */
+    public function __construct($config)
+    {
+        $this->connectionId = $this->connect($config);
+    }
+
+    /**
+     * Establish ftp connection
+     *
+     * @param $config
+     * @return resource
+     */
+    public function connect($config)
+    {
+        $connectionId = ftp_connect($config['host']);
+        $loginResponse = ftp_login($connectionId, $config['username'], $config['password']);
+        ftp_pasv($connectionId, $config['passive']);
+        ftp_set_option($connectionId, FTP_TIMEOUT_SEC, 300);
+
+        if ((!$connectionId) || (!$loginResponse))
+            die('FTP connection has failed!');
+
+        return $connectionId;
+    }
+
+    /**
+     * Disconnect active connection.
+     *
+     * @param  config
+     * @return void
+     */
+    public function disconnect()
+    {
+        ftp_close($this->connectionId);
+    }
+
 }
