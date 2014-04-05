616437f53db325ca24449001c15dbc86f7ebd372
Tufan Baris Yildirim <tufanbarisyildirim@gmail.com>
2014-04-05 10:32:01 +0300

    typo.

--- src/Anchu/Ftp/Ftp.php
+++ src/Anchu/Ftp/Ftp.php
@@ -30,8 +30,8 @@ class Ftp {
     {
         if(!isset($config['port']))
             $config['port'] = 21;
-        
-        $connectionId = ftp_connect($config['host']);
+
+        $connectionId = ftp_connect($config['host'],$config['port']);
         $loginResponse = ftp_login($connectionId, $config['username'], $config['password']);
         ftp_pasv($connectionId, $config['passive']);
         ftp_set_option($connectionId, FTP_TIMEOUT_SEC, 300);
