2f7c7ca312377a4245710c5d5e42cd307270ec9e
Tobias Kündig <tobias.kuendig@offlinegmbh.ch>
2015-04-19 16:57:02 +0200

    Added getDirListingDetailed() method

--- src/Anchu/Ftp/Ftp.php
+++ src/Anchu/Ftp/Ftp.php
@@ -79,6 +79,47 @@ class Ftp {
     }
 
     /**
+     * Get directory listing (detailed)
+     *
+     * @param string $directory
+     *
+     * @return array|bool
+     *
+     * @see      https://php.net/manual/de/function.ftp-rawlist.php#110803
+     *
+     */
+    public function getDirListingDetailed($directory = '.')
+    {
+        if (is_array($children = @ftp_rawlist($this->connectionId, $directory))) {
+            $items = array();
+
+            foreach ($children as $child) {
+                $chunks = preg_split("/\s+/", $child);
+                list(
+                    $item['rights'],
+                    $item['number'],
+                    $item['user'],
+                    $item['group'],
+                    $item['size'],
+                    $item['month'],
+                    $item['day'],
+                    $item['time']
+                ) = $chunks;
+
+                $item['type'] = $chunks[0]{0} === 'd' ? 'directory' : 'file';
+                array_splice($chunks, 0, 8);
+
+                $items[implode(" ", $chunks)] = $item;
+            }
+
+            return $items;
+        }
+
+        return false;
+
+    }
+
+    /**
      * Create new directory
      *
      * @param $directory
