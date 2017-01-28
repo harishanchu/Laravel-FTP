6f1533d6b491adddab9ac87989ca96dc04ec6fee
Harish Anchu <harishanchu@gmail.com>
2017-01-28 14:48:31 +0530

    Code format

--- src/Anchu/Ftp/FtpServiceProvider.php
+++ src/Anchu/Ftp/FtpServiceProvider.php
@@ -2,53 +2,51 @@
 
 use Illuminate\Support\ServiceProvider;
 
-class FtpServiceProvider extends ServiceProvider {
+class FtpServiceProvider extends ServiceProvider
+{
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
 
-	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-	protected $defer = false;
-
-	/**
-	 * Bootstrap the application events.
-	 *
-	 * @return void
-	 */
-	public function boot()
-	{
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
         $this->publishes([
-            __DIR__.'/../../config/config.php' => config_path('ftp.php'),
+            __DIR__ . '/../../config/config.php' => config_path('ftp.php'),
         ]);
-	}
+    }
 
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-        $this->app->singleton('ftp', function($app)
-        {
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->singleton('ftp', function ($app) {
             return new FtpManager($app);
         });
-        $this->app->booting(function()
-        {
+
+        $this->app->booting(function () {
             $loader = \Illuminate\Foundation\AliasLoader::getInstance();
             $loader->alias('FTP', 'Anchu\Ftp\Facades\Ftp');
         });
-	}
+    }
 
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides()
-	{
-		return array('ftp');
-	}
-
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return array('ftp');
+    }
 }
